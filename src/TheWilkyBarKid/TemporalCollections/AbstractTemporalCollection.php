5e8a8614f3f856735a1055fc62f5e8951ecb8bdd
thewilkybarkid <chriswilkinson84@gmail.com>
2014-03-29 20:45:07 +0000

    Add license

--- src/TheWilkyBarKid/TemporalCollections/AbstractTemporalCollection.php
+++ src/TheWilkyBarKid/TemporalCollections/AbstractTemporalCollection.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of the Temporal Collections library.
+ *
+ * (c) Chris Wilkinson
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace TheWilkyBarKid\TemporalCollections;
 
 use DateTime;
